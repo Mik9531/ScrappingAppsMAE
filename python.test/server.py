dbcd4e21a1b9455c7cba0c799f81cfc487fd5731
Miguel Afán Espinosa <miguelafannn@gmail.com>
2022-04-10 02:39:02 +0200

    Ejecucion de insert into en db

--- python.test/server.py
+++ python.test/server.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+import scraper
+
+import pymysql
+
+import array as arr
+
+
+def obtainList():
+    resultList = scraper.list('TOP_FREE', None, None, 500, 'es', 'US', True)
+    return resultList
 
 
 @app.route('/')
@@ -11,9 +21,34 @@ def index():
 
 @app.route('/listadoApks/')
 def my_link():
-    print('I got clicked!')
+    print('Enlace Clickado')
 
-    return 'Click.'
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='kalandria',
+                                 db='testpy',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
+
+    resultList = obtainList()
+
+    apps = ()
+
+    totalApps = []
+
+    for app in resultList:
+        apps = (app['appId'], app['title'])
+        totalApps.append(apps)
+
+    sql = "INSERT INTO Aplicaciones(appId,title,created) VALUES (%s,%s,NOW()) ON DUPLICATE KEY UPDATE updated = NOW()"
+    val = totalApps
+    cursor.executemany(sql, val)
+    connection.commit()
+    print(cursor.rowcount, "record inserted.")
+    connection.close()
+
+    return 'Terminado'
 
 
 if __name__ == '__main__':
