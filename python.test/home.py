970029f6bad15ea006801c6e7df0624b25b60c57
Miguel Afán Espinosa <miguelafannn@gmail.com>
2022-05-01 19:32:15 +0200

    Enlaces en home

--- python.test/home.py
+++ python.test/home.py
@@ -1,22 +1,12 @@
 # coding=utf8
-from sqlalchemy import create_engine
-import pandas as pd
-import dash_bootstrap_components as dbc
-from app import app
+
 from dash import Dash, dcc, html, Input, Output
-import plotly.graph_objects as go
-from datetime import date
-from datetime import datetime
-
-# Importamos los datos desde nuestro mysql
-
 
 # ------------------------------------------------------------------------------
-# App layout
 
 
 home_layout = html.Div([
 
-    html.H1("Aplicación web con Dash Home", style={'text-align': 'center'})
+    html.H1("Aplicación web con Dash Home", style={'text-align': 'center'}),
 
 ])
--- python.test/index.py
+++ python.test/index.py
@@ -8,20 +8,40 @@ from top_grossing import top_grossing_layout
 from home import home_layout
 
 # ------------------------------------------------------------------------------
-app_tabs = html.Div(
-    [
-        dbc.Tabs(
-            [
-                dbc.Tab(label="Home", tab_id="tab-home", labelClassName="text-success font-weight-bold",
-                        activeLabelClassName="text-danger"),
-                dbc.Tab(label="Mapa Ingresos", tab_id="tab-grossing", labelClassName="text-success font-weight-bold",
-                        activeLabelClassName="text-danger"),
-            ],
-            id="tabs",
-            active_tab="tab-home",
-        ),
-    ], className="mt-3"
-)
+# app_tabs = html.Div(
+#     [
+#         dbc.Tabs(
+#             [
+#                 dbc.Tab(label="Home", tab_id="tab-home", labelClassName="text-success font-weight-bold",
+#                         activeLabelClassName="text-danger"),
+#                 dbc.Tab(label="Mapa Ingresos", tab_id="tab-grossing", labelClassName="text-success font-weight-bold",
+#                         activeLabelClassName="text-danger"),
+#             ],
+#             id="tabs",
+#             active_tab="tab-home",
+#         ),
+#     ], className="mt-3"
+# )
+
+# link_tabs_layout = html.Div(
+#     dbc.Row(
+#         [
+#             [
+#                 dcc.Link('Go to Page 1', href='/top-grossing'),
+#
+#                 dcc.Link('Go to Page 2', href='/top-grossing'),
+#
+#             ]
+#         ])
+# )
+
+link_tabs_layout = html.Div([
+    dbc.Row([
+        dbc.Col(dcc.Link('Home', href='/'), width=3),
+        dbc.Col(dcc.Link('Mapa Ingresos', href='/top-grossing'), width=3)
+    ]
+    )
+])
 
 app.layout = html.Div([
     dbc.Row(
@@ -33,7 +53,7 @@ app.layout = html.Div([
                         [
 
                             dbc.Col(html.H5("APLICACIÓN WEB CON DASH HOME"), width=6, className="top_bar"),
-                            dbc.Col(app_tabs, width=6, className="index_tabs", align='right'),
+                            dbc.Col(link_tabs_layout, width=6, className="index_tabs", align='right'),
                         ]
                     ),
                 ),
@@ -45,20 +65,25 @@ app.layout = html.Div([
     ),
     html.Hr(),
 
-    html.Div(id='tabs_content', children=[], className="cards")
+    dcc.Location(id='url', refresh=False),
+
+    html.Div(id='page-content')
 
 ])
 
 
 @app.callback(
-    Output("tabs_content", "children"),
-    [Input("tabs", "active_tab")]
+    Output("page-content", "children"),
+    Input('url', 'pathname')
 )
-def switch_tab(tab_chosen):
-    if tab_chosen == "tab-home":
+def switch_tab(pathname):
+    print(pathname)
+    if pathname == '/home' or pathname == '/':
         return home_layout
-    elif tab_chosen == "tab-grossing":
+    elif pathname == '/top-grossing':
         return top_grossing_layout
+
+    # You could also return a 404 "URL not found" page here
 
 
 if __name__ == '__main__':
