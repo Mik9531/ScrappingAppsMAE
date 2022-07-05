9a0fc90bbf31fd45e152a54beb5a7c3122efc3b2
Miguel Afán Espinosa <miguelafannn@gmail.com>
2022-07-05 21:24:15 +0200

    Cambios en aplicaciones

--- appsDashboard/totalApps.py
+++ appsDashboard/totalApps.py
@@ -19,18 +19,6 @@ last_date = last_date['created'].values[0]
 # ------------------------------------------------------------------------------
 # App layout
 
-# data = OrderedDict(
-#     [
-#         ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
-#         ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
-#         ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
-#         ("Humidity", [10, 20, 30, 40, 50, 60]),
-#         ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
-#     ]
-# )
-#
-# df = pd.DataFrame(data)
-
 apps_layout = html.Div([
 
     dbc.Row(
@@ -67,58 +55,14 @@ apps_layout = html.Div([
                                 [
 
                                     dbc.Row(
-                                        [
 
-                                            dbc.Col(
+                                        dbc.Col(
 
-                                                html.Img(id='output_img', height='350px', width='350px'),
-                                                width={'size': 7, "offset": 0, 'order': 1}
-                                            ),
+                                            html.Img(id='output_img', height='350px', width='350px'),
 
-                                            dbc.Col(
-                                                [
+                                        ),
 
-                                                    dbc.Row(
-
-                                                        html.H5(id="output_title", className="card-title",
-                                                                ),
-
-                                                    ),
-                                                    html.Br(),
-
-                                                    dbc.Row(
-
-                                                        html.P(id="output_summary", className="card-title",
-                                                               ),
-
-                                                    ),
-                                                    html.Br(),
-
-                                                    dbc.Row(
-
-                                                        html.P(id="output_score", className="card-title",
-                                                               ),
-
-                                                    ),
-                                                    html.Br(),
-
-                                                    dbc.Row(
-
-                                                        html.P(id="output_developer", className="card-title",
-                                                               ),
-
-                                                    ),
-                                                    html.Br(),
-
-                                                    dbc.Row(
-
-                                                        html.Div(id="output_url"
-
-                                                                 ))],
-
-                                                width={'size': 5, "offset": 0, 'order': 1}
-                                            ),
-                                        ])
+                                    )
 
                                 ]
                             ),
@@ -126,48 +70,254 @@ apps_layout = html.Div([
                         )
                     ]
                 ),
-                width={'size': 5, "offset": 0, 'order': 1}
+                width={'size': 4, "offset": 0, 'order': 1}
             ),
             dbc.Col(dbc.Card(
                 dbc.CardBody(
                     [
-                        dbc.CardHeader("Puntuación de la aplicación en el tiempo"),
-                        dcc.Graph(id='apps_map2', figure={})
+                        dbc.CardHeader("Datos de la aplicación"),
+                        dbc.Row([
+
+                            dbc.Col(
+                                [
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Nombre de la aplicación", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_title", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Resumen", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_summary", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Puntuación", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_score", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Desarrollador", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_developer", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Instalaciones", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_installs", className="card-title",
+                                               ),
+
+                                    ),
+
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.Div(id="output_url"
+
+                                                 ))],
+
+                                width={'size': 6, "offset": 0, 'order': 0}
+                            ),
+                            dbc.Col(
+                                [
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Precio", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_price", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Versión soportada", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_androidVersionText", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Género", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_genre", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Clasificación del contenido", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_contentRating", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                    dbc.Row(
+
+                                        html.H6("Cambios recientes", className="card-title",
+                                                ),
+
+                                    ),
+
+                                    dbc.Row(
+
+                                        html.P(id="output_recentChanges", className="card-title",
+                                               ),
+
+                                    ),
+                                    html.Br(),
+
+                                ],
+
+                                width={'size': 6, "offset": 0, 'order': 0}
+                            )
+
+                        ],
+
+                        )
                     ]
                 ),
                 className="cards"
 
             ),
-                width={'size': 7, "offset": 0, 'order': 2}
+                width={'size': 8, "offset": 0, 'order': 2}
             ),
         ]
     ),
 
     dbc.Card(
         dbc.CardBody(
-            dbc.Row(
-                dash_table.DataTable(
-                    id='rowsPermit',
-                    columns=[
-                        {'name': 'Localización', 'id': 'Location'},
-                        {'name': 'Calendario', 'id': 'Calendar'},
-                        {'name': 'Micrófono', 'id': 'Microphone'},
-                        {'name': 'Contactos', 'id': 'Contacts'},
-                        {'name': 'Historial', 'id': 'DeviceHistory'},
-                        {'name': 'Cámara', 'id': 'Camera'},
-                        {'name': 'Almacenamiento', 'id': 'Storage'},
-                        {'name': 'WiFi', 'id': 'WiFi'},
-                        {'name': 'Acceso a media', 'id': 'PhotosMediaFiles'},
-                        {'name': 'Teléfono', 'id': 'Phone'},
-                        {'name': 'ID Dispositivo', 'id': 'DeviceID'},
-                        {'name': 'SMS', 'id': 'SMS'},
-                        {'name': 'Identidad', 'id': 'Identity'},
-                    ],
-                    style_cell={'textAlign': 'center'},
-                    data=[{'rowsPermit': i} for i in range(1)],
-                    editable=True,
-                ), )
-        ))
+            [
+                dbc.CardHeader("Permisos de la aplicación"),
+                dbc.Row(
+                    dash_table.DataTable(
+                        id='rowsPermit',
+                        columns=[
+                            {'name': 'Localización', 'id': 'Location'},
+                            {'name': 'Calendario', 'id': 'Calendar'},
+                            {'name': 'Micrófono', 'id': 'Microphone'},
+                            {'name': 'Contactos', 'id': 'Contacts'},
+                            {'name': 'Historial', 'id': 'DeviceHistory'},
+                            {'name': 'Cámara', 'id': 'Camera'},
+                            {'name': 'Almacenamiento', 'id': 'Storage'},
+                            {'name': 'WiFi', 'id': 'WiFi'},
+                            {'name': 'Acceso a media', 'id': 'PhotosMediaFiles'},
+                            {'name': 'Teléfono', 'id': 'Phone'},
+                            {'name': 'ID Dispositivo', 'id': 'DeviceID'},
+                            {'name': 'SMS', 'id': 'SMS'},
+                            {'name': 'Identidad', 'id': 'Identity'},
+                        ],
+                        style_cell={'textAlign': 'center'},
+                        data=[{'rowsPermit': i} for i in range(1)],
+                        editable=True,
+                    ), )
+            ])),
+
+    dbc.Card(
+        dbc.CardBody(
+            [
+                dbc.CardHeader("Reseñas de la aplicación"),
+                dbc.Row(
+                    dash_table.DataTable(
+                        id='rowsPermit',
+                        columns=[
+                            {'name': 'Localización', 'id': 'Identity'},
+                            {'name': 'Calendario', 'id': 'Identity'},
+                            {'name': 'Micrófono', 'id': 'Identity'},
+                            {'name': 'Contactos', 'id': 'Identity'},
+                            {'name': 'Historial', 'id': 'Identity'},
+                            {'name': 'Cámara', 'id': 'Identity'},
+                            {'name': 'Almacenamiento', 'id': 'Identity'},
+                            {'name': 'WiFi', 'id': 'Identity'},
+                            {'name': 'Acceso a media', 'id': 'Identity'},
+                            {'name': 'Teléfono', 'id': 'Identity'},
+                            {'name': 'ID Dispositivo', 'id': 'Identity'},
+                            {'name': 'SMS', 'id': 'Identity'},
+                            {'name': 'Identidad', 'id': 'Identity'},
+                        ],
+                        style_cell={'textAlign': 'center'},
+                        data=[{'rowsPermit': i} for i in range(1)],
+                        editable=True,
+                    ), )
+            ]))
 
 ])
 
@@ -182,6 +332,12 @@ apps_layout = html.Div([
      Output('output_score', 'children'),
      Output('output_developer', 'children'),
      Output('output_url', 'children'),
+     Output('output_installs', 'children'),
+     Output('output_price', 'children'),
+     Output('output_androidVersionText', 'children'),
+     Output('output_genre', 'children'),
+     Output('output_contentRating', 'children'),
+     Output('output_recentChanges', 'children'),
      Output('rowsPermit', 'data')],
     Input(component_id='slct_app', component_property='value')
 )
@@ -199,12 +355,26 @@ def update_graph(app_selected):
     dff = dff.iloc[-1:]
 
     if dff.empty is False:
+        scoreFormat = "{:.2f}".format(dff.score.values[0])
+        priceFormat = "{:.2f}".format(dff.price.values[0])
         url_img = dff.icon.values[0]
         app_title = dff.title.values[0]
         app_summary = dff.summary.values[0]
-        app_score = dff.score.values[0]
+        app_score = '%s ⭐' % scoreFormat
         app_developer = dff.developer.values[0]
         app_url = dff.url.values[0]
+        app_installs = dff.installs.values[0]
+        price = '%s €' % priceFormat
+
+        if dff.androidVersionText.values[0] == 'Varies with device':
+            androidVersionText = 'Varía según el dispositivo'
+        else:
+            androidVersionText = dff.androidVersionText.values[0]
+
+        genre = dff.genre.values[0]
+        contentRating = dff.contentRating.values[0]
+        recentChanges = dff.recentChanges.values[0]
+
     else:
         url_img = "https://plantillasdememes.com/img/plantillas/imagen-no-disponible01601774755.jpg"
         app_title = "No disponible"
@@ -212,6 +382,12 @@ def update_graph(app_selected):
         app_score = "No disponible"
         app_developer = "No disponible"
         app_url = "/"
+        app_installs = "No disponible"
+        price = "No disponible"
+        androidVersionText = "No disponible"
+        genre = "No disponible"
+        contentRating = "No disponible"
+        recentChanges = "No disponible"
 
     div_url = html.Div(dcc.Link(DashIconify(
         icon="logos:google-play",
@@ -303,4 +479,4 @@ def update_graph(app_selected):
 
     rows = permitsApp
 
-    return url_img, app_title, app_summary, app_score, app_developer, div_url, rows
+    return url_img, app_title, app_summary, app_score, app_developer, div_url, app_installs, price, androidVersionText, genre, contentRating, recentChanges, rows
