38d87a9154ec78aef201edd1a9d88113475679f5
Miguel Afán Espinosa <miguelafannn@gmail.com>
2022-07-20 00:42:18 +0200

    Funcional con gran carga de trabajo

--- appsDashboard/boot.sh
+++ appsDashboard/boot.sh
@@ -4,4 +4,4 @@
 # go to directory where wsgi.py is
 cd /home/dashboard
 # start gunicorn
-exec gunicorn -b 0.0.0.0:8080 --workers 1 --threads 8 --timeout 0 app:app
+exec gunicorn -b 0.0.0.0:8080 --workers 1 --threads 1 --timeout 600 app:app
