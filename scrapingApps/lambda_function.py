69371f9176e4caaba1610c9934b836acecdc6689
Miguel Afán Espinosa <miguelafannn@gmail.com>
2022-08-03 19:04:35 +0200

    Cambios generales

--- scrapingApps/lambda_function.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import Request, urlopen  # Python 3
-
-
-req = Request('http://dockerflaskpython-env.eba-4mvjtpq7.us-east-2.elasticbeanstalk.com/')
-content = urlopen(req).read()
-
-print (content)
